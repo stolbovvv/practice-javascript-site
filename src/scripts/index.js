28f823fa19fc2547b796f943aa70ddab309c8a6e
Valerii Stolbov <stolbov.v@outlook.com>
2023-08-08 19:47:02 +0500

    create class

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -139,4 +139,72 @@ window.addEventListener('DOMContentLoaded', () => {
   window.addEventListener('scroll', showModalByScroll);
 
   modalTimerId = setTimeout(showModal, 60000);
+
+  // CLASS
+  class MenuCard {
+    constructor({ src, alt, title, descr, price, parentSelector }) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.descr = descr;
+      this.price = price;
+      this.parent = document.querySelector(parentSelector);
+      this.transfer = 95;
+
+      this.converToRUB();
+    }
+
+    converToRUB() {
+      this.price = this.price * this.transfer;
+    }
+
+    render() {
+      const element = document.createElement('div');
+
+      element.innerHTML = `
+        <div class="menu__item">
+          <img src="${this.src}" alt="${this.alt}" />
+          <h3 class="menu__item-subtitle">${this.title}</h3>
+          <div class="menu__item-descr">${this.descr}</div>
+          <div class="menu__item-divider"></div>
+          <div class="menu__item-price">
+            <div class="menu__item-cost">Цена:</div>
+            <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
+          </div>
+        </div>
+      `;
+
+      this.parent.append(element);
+    }
+  }
+
+  new MenuCard({
+    src: './images/tabs/vegy.jpg',
+    alt: 'vegy',
+    title: 'Меню “Фитнес“',
+    descr:
+      'Меню “Фитнес“ - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    price: 10,
+    parentSelector: '.menu__field .container',
+  }).render();
+
+  new MenuCard({
+    src: './images/tabs/elite.jpg',
+    alt: 'elite',
+    title: 'Меню “Премиум”',
+    descr:
+      'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+    price: 25,
+    parentSelector: '.menu__field .container',
+  }).render();
+
+  new MenuCard({
+    src: './images/tabs/post.jpg',
+    alt: 'post',
+    title: 'Меню “Постное“',
+    descr:
+      'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+    price: 18,
+    parentSelector: '.menu__field .container',
+  }).render();
 });
